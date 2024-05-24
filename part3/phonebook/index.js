5b31f352defa5a8650373bca9b58d6a665457d7a
Jorge Amat Soler <amjsoler@gmail.com>
2024-05-24 13:54:57 +0200

    part3: 3.1 - 3.6

---
+++ part3/phonebook/index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const { v4: uuidv4 } = require('uuid');
+var bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+let persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+});
+
+app.get('/info', (req, res) => {
+const date = new Date()
+    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if(person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.post('/api/persons', (req, res) => {
+    if(!req.body.name || !req.body.number) {
+        return res.status(422).json({
+            error: 'name or number is missing'
+        })
+    }
+
+    if(persons.find(person => person.name === req.body.name)) {
+        return res.status(422).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: uuidv4(),
+        name: req.body.name,
+        number: req.body.number
+    }
+
+    persons = persons.concat(person)
+
+    res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+    res.status(200).end()
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
